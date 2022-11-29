4303437189311b7569f11d169f0c81a8cf2698fe
Zubaier <zubaier10@hotmail.com>
2022-11-29 23:25:24 +0100

    Added rest endpoints

--- src/main/java/no/sas/controller/PersonController.java
+++ src/main/java/no/sas/controller/PersonController.java
@@ -3,9 +3,7 @@ package no.sas.controller;
 import no.sas.models.Passenger;
 import no.sas.service.PassengerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,13 +13,23 @@ public class PersonController {
     @Autowired
     private PassengerService passengerService;
 
-    @RequestMapping("/passengers")
+    @RequestMapping(method=RequestMethod.GET, value="/passengers")
     public List<Passenger> getPassengers(){
-        return passengerService.getPassengerList();
+        return this.passengerService.getPassengerList();
     }
 
-    @RequestMapping("/passengers/{id}")
-    public Passenger getDetails(@PathVariable Passenger passenger){
-        return passengerService.getPassengerList().stream().filter(p -> p.equals(passenger)).findFirst().get();
+    @RequestMapping(method=RequestMethod.GET, value="/passengers/{passenger}")
+    public Passenger getPassenger(@PathVariable String passenger){
+        return this.passengerService.getPassenger(passenger);
+    }
+
+    @RequestMapping(method=RequestMethod.POST, value="/passengers")
+    public void addPassenger(@RequestBody Passenger passenger){
+        this.passengerService.addPassenger(passenger);
+    }
+
+    @RequestMapping(method=RequestMethod.PUT, value="/passengers/{passengerSocialSecNumber}")
+    public void updatePassenger(@RequestBody Passenger passenger, @PathVariable String passengerSocialSecNumber){
+        this.passengerService.updatePassenger(passenger, passengerSocialSecNumber);
     }
 }
